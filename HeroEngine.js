1376791525da476455d8f57f4484b754223c65e3
FranklyBrandon <bran.frankenfield@gmail.com>
2019-10-24 21:10:35 -0500

    Add event emitter and event for main game loop

--- HeroEngine.js
+++ HeroEngine.js
@@ -1,50 +1,63 @@
-class Room {
+class Game {
 
-    constructor() {
-    this.lastRender = 0;
-    this.input = new Input();
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        this.lastRender = 0;
+        this.input = new Input();
+        this.canvas = document.getElementById("heroEngineCanvas");
+        this.context = this.canvas.getContext("2d");
+
+        this.events = new EventEmitter();
+        this.events.registerEvent('STEP');
+        this.events.addEventListener('STEP', function(eventArgs) {
+        console.log('Step event has been triggered');
+        });
     }
-    
+
     update(delta) {
-        console.log('Frame has updated!');
+        this.events.emitEvent('STEP', delta);
     }
-    
+
     draw() {
-    
+
     }
-    
+
     loop(timestamp) {
-     let delta = timestamp - this.lastRender;
-     console.log(delta);
-    
-     this.update(delta);
-     this.draw();
-    
-     this.lastRender = timestamp;
-     
-     let loop = this.loop.bind(this);
-     window.requestAnimationFrame(loop);
+        let delta = timestamp - this.lastRender;
+
+        this.update(delta);
+        this.draw();
+
+        this.lastRender = timestamp;
+
+        let loop = this.loop.bind(this);
+        window.requestAnimationFrame(loop);
     }
-    
+
     start() {
-    let loop = this.loop.bind(this);
-    window.requestAnimationFrame(loop);
+        let loop = this.loop.bind(this);
+        window.requestAnimationFrame(loop);
     }
-    }
-    
+}
+
 class Input {
     constructor(){
 
     }
-    
-    InitKeyboard(){   
+
+    InitKeyboard(){
+
+        //Data Structure to store KeyCode maps
         this.KeyMap = {
+            //KeyCode to Human readable
             KeyCode: {
                 68: 'D',
                 65: 'A',
                 87: 'W',
                 83: 'S'
             },
+            //Human readable to KeyCode
             VirtualKey: {
                 'D': 68,
                 'A': 65,
@@ -52,32 +65,82 @@ class Input {
                 'S': 83
             }
         };
-        
+
         this.Keyboard = [];
-        
-        window.addEventListener("keydown", this.Keydown, false)
-        window.addEventListener("keyup", this.Keyup, false)
+
+        //Bind keydown and keyup event listeners
+        let keydownFunc = this.Keydown.bind(this);
+        let keyupFunc = this.Keyup.bind(this);
+        window.addEventListener("keydown", keydownFunc, false)
+        window.addEventListener("keyup", keyupFunc, false)
     }
-    
+
+    //Event fired when a key is pressed
     Keydown(event) {
-        console.log('key down event has been fired');
+        let keyObj = this.Keyboard[event.keyCode];
+        if(keyObj) keyObj.IsPressed = true;
     }
-    
+
+    //Event fired when a key is released
     Keyup(event) {
-        console.log('key up event has been fired');
+        let keyObj = this.Keyboard[event.keyCode];
+        if(keyObj) keyObj.IsPressed = false;
     }
-    
+
+    //Add a key to listen to
     AddKey(key) {
-        this.Keyboard.push({
-        key: 'MyKeyObject'
-        });
+        let keyObj = new KeyObj(key);
+        let keyCode = this.KeyMap.VirtualKey[key];
+        this.Keyboard[keyCode] = keyObj;
     }
 }
-    
-    
-    var scene = new Room();
-    scene.start();
-    scene.input.InitKeyboard();
-    scene.input.AddKey('D');
-    
-    
+class KeyObj {
+
+    constructor(key){
+        this.Key = key;
+        this.IsPressed = false;
+    }
+}
+
+class Event {
+    constructor(name){
+        this.name = name;
+        this.callbacks = [];
+    }
+
+    registerCallback(callback) {
+        this.callbacks.push(callback);
+    }
+}
+
+class EventEmitter {
+    constructor() {
+        this.events = [];
+    }
+
+    registerEvent(eventName) {
+        let event = new Event(eventName);
+        this.events[eventName] = event;
+    }
+
+    emitEvent(eventName, eventArgs) {
+        let event = this.events[eventName];
+        if (event)
+        {
+            event.callbacks.forEach(function(callback){
+                callback(eventArgs);
+            });
+        }
+    }
+
+    addEventListener(eventName, callback) {
+        let event = this.events[eventName];
+        if (event) event.registerCallback(callback);
+    }
+}
+
+
+var game = new Game(200, 100);
+game.input.InitKeyboard();
+game.input.AddKey('D');
+game.start();
